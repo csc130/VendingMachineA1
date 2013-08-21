886644458d1362a0a9477313f86bde3a2baac293
tkanchanawanchai6403 <tkanchanawanchai6403@AN098046-CT103I.nvstu.edu>
2013-08-21 17:07:16 -0400

    with change

--- Assignment1/src/HelloVending.java
+++ Assignment1/src/HelloVending.java
@@ -7,6 +7,7 @@ public class HelloVending {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Vending!");
+		System.out.println("change");
 	}
 
 }
